256ba7ede292ad9ab156b5ec3737850e1bc0e809
BorhanPetgar <borhanpetgar40@gmail.com>
2024-05-27 22:20:58 +0330

    resnet classes added

---
+++ resnet/resnet.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class BasicBlock(nn.Module):
+    
+    def __init__(self):
+        super(BasicBlock, self).__init__()
+        # init
+        
+    def forward(self, x):
+        identity = x
+        # rest of the code
+        
+
+class Resnet18(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        
+        
+    def forward(self, x):
+        pass
